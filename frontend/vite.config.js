3d2b126ae649da7717e833e68f13dba59889be6b
Mike <124715146+Nasokom@users.noreply.github.com>
2026-07-31 18:17:47 +0200

    first commit base App

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./testSetup.js",
+  },
+  server: {
+    allowedHosts: ["app"],
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080/api",
+        changeOrigin: true,
+      },
+    },
+  },
+});
